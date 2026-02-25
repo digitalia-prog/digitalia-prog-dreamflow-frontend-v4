427062616106fab21f6464e6861f8151e2276bc5
AITEUR <aiteur@gmail.com>
2026-02-25 06:34:59 +0100

    fix next-intl config

--- app/request.ts
+++ app/request.ts
@@ -1,14 +1,14 @@
 import {getRequestConfig} from "next-intl/server";
-import {locales, type Locale, defaultLocale} from "../i18n";
+import {locales, defaultLocale, type Locale} from "../i18n";
 
 export default getRequestConfig(async ({requestLocale}) => {
-  let locale = (await requestLocale) as Locale;
-
-  if (!locales.includes(locale)) locale = defaultLocale;
+  const locale =
+    locales.includes(requestLocale as Locale)
+      ? (requestLocale as Locale)
+      : defaultLocale;
 
   return {
     locale,
     messages: (await import(`../messages/${locale}.json`)).default
   };
 });
-

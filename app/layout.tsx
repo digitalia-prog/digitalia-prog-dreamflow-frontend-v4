eec76626de8461c03ce20106157945436f9ea74f
AITEUR <aiteur@gmail.com>
2026-02-25 08:17:59 +0100

    fix: build + i18n stable

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { NextIntlClientProvider } from "next-intl";
 import { getLocale, getMessages } from "next-intl/server";
-import "../globals.css";
+import "./globals.css"
 
 import LanguageSwitcher from "../components/LanguageSwitcher";
 

576521882b75fb4fab578be2a56f6037a5ec1b1f
AITEUR <aiteur@gmail.com>
2026-02-25 06:08:04 +0100

    fix locales export for language switcher

--- i18n.ts
+++ i18n.ts
@@ -1,6 +1,9 @@
-import {getRequestConfig} from 'next-intl/server';
+import {getRequestConfig} from "next-intl/server";
+
+export const locales = ["fr", "en"] as const;
+export type Locale = (typeof locales)[number];
 
 export default getRequestConfig(async () => ({
-  locale: 'fr',
-  messages: (await import('./messages/fr.json')).default
+  locale: "fr",
+  messages: (await import("./messages/fr.json")).default,
 }));

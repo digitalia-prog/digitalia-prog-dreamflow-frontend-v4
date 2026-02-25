88d64d16069d0faf30649d881c71424586a732cd
AITEUR <aiteur@gmail.com>
2026-02-25 06:40:35 +0100

    fix requestLocale promise

--- app/request.ts
+++ app/request.ts
@@ -2,9 +2,11 @@ import {getRequestConfig} from "next-intl/server";
 import {locales, defaultLocale, type Locale} from "../i18n";
 
 export default getRequestConfig(async ({requestLocale}) => {
-  const locale =
-    locales.includes(requestLocale as Locale)
-      ? (requestLocale as Locale)
+  const requested = await requestLocale;
+
+  const locale: Locale =
+    locales.includes(requested as Locale)
+      ? (requested as Locale)
       : defaultLocale;
 
   return {

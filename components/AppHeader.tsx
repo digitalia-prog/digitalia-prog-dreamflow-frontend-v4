16f2d1958360a587a5b275dac7ad7f95e35e80b6
AITEUR <aiteur@gmail.com>
2026-03-12 00:40:24 +0100

    remove duplicate language switcher from header

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -1,9 +1,6 @@
-import LanguageSwitcher, { Locale } from "./LanguageSwitcher";
-
-export default function AppHeader({ locale }: { locale: Locale }) {
+export default function AppHeader() {
   return (
     <header className="w-full flex items-center justify-end px-6 py-3 bg-zinc-950 border-b border-zinc-800">
-      <LanguageSwitcher value={locale} />
     </header>
   );
 }
